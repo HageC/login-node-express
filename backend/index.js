f350a7743971a1d096d8a53dca7dcc879cffe50b
Christopher Hage <hage.chris@yahoo.com>
2023-07-24 18:05:48 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import mongoose from "mongoose";
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(process.env.SECRET);
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+const start = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  } catch (error) {}
+  console.log(process.env.SECRET);
+};
+
+start();
